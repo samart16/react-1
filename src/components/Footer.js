363edee1b047ca7047062e808a9982b731c343c3
Steven Martinez <smartinez1092@gmail.com>
2023-03-23 11:36:55 -0500

    Final Design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -28,14 +28,65 @@ const Footer = () => {
             <div className='footer-link-wrapper'>
                 <div className='footer-link-items'>
                     <h2>About us</h2>
-                    <Link></Link>
-                    <Link></Link>
-                    <Link></Link>
-                    <Link></Link>
+                    <Link to='/'>Testimonials</Link>
+                    <Link to='/'>Careers</Link>
+                    <Link to='/'>Investors</Link>
+                    <Link to='/'>Terms of Service</Link>
+                </div>
+                <div className='footer-link-items'>
+                    <h2>About us</h2>
+                    <Link to='/'>Testimonials</Link>
+                    <Link to='/'>Careers</Link>
+                    <Link to='/'>Investors</Link>
+                    <Link to='/'>Terms of Service</Link>
+                </div>
+            </div>
+            <div className='footer-link-wrapper'>
+                <div className='footer-link-items'>
+                    <h2>About us</h2>
+                    <Link to='/'>Testimonials</Link>
+                    <Link to='/'>Careers</Link>
+                    <Link to='/'>Investors</Link>
+                    <Link to='/'>Terms of Service</Link>
+                </div>
+                <div className='footer-link-items'>
+                    <h2>About us</h2>
+                    <Link to='/'>Instagram</Link>
+                    <Link to='/'>Facebook</Link>
+                    <Link to='/'>Twitter</Link>
                 </div>
             </div>
         </div>
-
+        <section className='social-media'>
+            <div className='social-media-wrap'>
+                <div className='footer-logo'>
+                    <Link  className='social-logo' to='/'>TRVL</Link>
+                </div>
+                <div className='social-icons'>
+                    <Link className='social-icon-link facebook'
+                    to='/'
+                    target='_blank'
+                    aria-label='Facebook'
+                    >
+                        <i className='fa fa-facebook-square'></i>
+                    </Link>
+                    <Link className='social-icon-link instagram'
+                    to='/'
+                    target='_blank'
+                    aria-label='Instagram'
+                    >
+                        <i className='fa fa-instagram'></i>
+                    </Link>
+                    <Link className='social-icon-link twitter'
+                    to='/'
+                    target='_blank'
+                    aria-label='Twitter'
+                    >
+                        <i className='fa fa-twitter-square'></i>
+                    </Link>
+                </div>
+            </div>
+        </section>
     </div>
   )
 }
